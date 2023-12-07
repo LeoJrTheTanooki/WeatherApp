6a58248877c1e60cb02c2e49cd03c7ce7be7476f
LeoJrTheTanooki <aceraccoon@proton.me>
2023-12-06 16:14:51 -0800

    See 12/6 README.md

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
+# Leo Garcia
+
+# Weather App
+
+Making a weather app based on given Figma
+
+# Figma
+
+https://www.figma.com/file/p1BcHrGsq1wEIrLkMJlSzg/Untitled?type=design&node-id=10-50&mode=design&t=SiwtA6WyP5LldA5J-0c
+
+#
+
 # 12/4/23
 
-### Standup
+## Standup
 
 * Go over Figma and gather necessary assets
 * Set up HTML and structure of website
@@ -8,7 +20,7 @@
 * Make API fetches show in console
 * Listen to Vercel lecture
 
-### End of day report
+## End of day report
 
 * Listened to morning lecture
 * Worked on setting up necessary files
@@ -25,30 +37,56 @@
 * Changed API from forecast to weather, edited declarations to reflect changes
 * Adjusted layout to reflect Figma more
 
-### Needs Work
+## Needs Work
 
 * Culling images
 * Fixing naming conventions of API
 * Culling state name
 
+### Special thanks to Caleb and Elizabeth for assistance with Fetch key security and API respectively
+
 #
 
 # 12/5/2023
 
-### Standup
-Adjust layout to reflect Figma more
-Add in interactable elements (scrolling on right column, dropdown search bar and following days forecast, favorite button)
-Assign id to necessary elements via span
+## Standup
+* Adjust layout to reflect Figma more
+* Add in interactable elements (scrolling on right column, dropdown search bar and following days forecast, favorite button)
+* Assign id to necessary elements via span
 
-### End of day report
+## End of day report
 * Rounded corners, added shadows, adjusted font
 * Adjusted image positions and sizes
 * Made scrollable container to right column
 * Reorganized columns and rows
 * Added span ids
 
-### Needs Work
+## Needs Work
 
 * Dropdown menus on right column
 * Write to spans via Javascript
 * Move '°F|°C' to align to top
+
+# 12/6/23
+
+## Standup
+* Work on dropdown menus
+* Assign elements in Javascript
+* Make text elements change
+* Make image elements change
+* Implement saving coordinates to array
+
+## End of day report
+* Made dropdown menu
+* Assigned elements in Javascript
+* Made save button change image
+* Made HTML Text change according to weather API
+* Made weather icon change according to API
+* Worked on for loop for forecast
+
+## Needs work
+* Finishing for loop
+* Finishing layout
+* Changing location system
+* Search function
+* Saving coordinates to array
--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,9 @@
 // Fetch Weather API
 // Seperate and log element arrays
 // Assign elements to given text and images
+// Use getElementById() to assign
+// Use getElementById().textContent display
+// For images use a switch case
 // Create search function
 // Create save function set to save cities in an array
 
@@ -10,6 +13,26 @@ import { apiKey } from "./environment.js";
 console.log("Gaming");
 let lat;
 let lon;
+let f_toggle;
+let c_toggle;
+let favBtn = document.getElementById("favBtn");
+let fMode = true;
+let locSaved = false;
+
+favBtn.addEventListener("click", function () {
+  favToggle();
+});
+
+function favToggle() {
+  console.log("Favorite game is Rhythm Heaven");
+  if (!locSaved) {
+    favBtn.src = "./assets/star.png";
+    locSaved = true;
+  } else {
+    favBtn.src = "./assets/starline.png";
+    locSaved = false;
+  }
+}
 
 navigator.geolocation.getCurrentPosition(success, errorFunc);
 
@@ -27,51 +50,114 @@ function success(position) {
   lon = position.coords.longitude;
 
   async function apiCall() {
+    if (fMode) {
+    }
+
     const promise = await fetch(
       `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&id=${apiKey}&units=imperial`
     );
-    // const promise = await fetch(
-    //   `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&id=${apiKey}&units=imperial`
-    // );
 
     const data = await promise.json();
-    let temp = data.main.temp;
-    let temp_min = data.main.temp_min;
-    let temp_max = data.main.temp_max;
+    let temp = Math.round(data.main.temp);
+    let temp_min = Math.round(data.main.temp_min);
+    let temp_max = Math.round(data.main.temp_max);
     let city = data.name;
     let state = data.state;
     let country = data.sys.country;
     let weather = data.weather[0].main;
     let conditions = data.weather[0].description;
     let icon = data.weather[0].icon;
-    let wind = data.wind.speed;
+    let wind = Math.round(data.wind.speed);
     let humidity = data.main.humidity;
     let location = state ? `${city}, ${state}` : `${city}, ${country}`;
+
     //   let tempC;
     //   let temp_minC;
     //   let temp_maxC;
 
     switch (weather) {
+      case "Clear":
+        weather = "Clear Sky";
+        break;
       case "Clouds":
         weather = "Cloudy";
         break;
     }
-    // console.log(
-    //   `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&id=${apiKey}&units=imperial`
-    // );
     console.log(data);
     console.log(location);
     console.log(`Weather: ${weather}`);
     console.log(`Conditions: ${conditions}`);
     console.log(`Icon: ${icon}`);
     console.log(`Wind speed: ${wind}`);
-    console.log(`Wind speed: ${humidity}`);
-    console.log(`Temperature: ${Math.round(temp)}°F`);
-    console.log(`Min Temp: ${Math.round(temp_min)}°F`);
-    console.log(`Max Temp: ${Math.round(temp_max)}°F`);
+    console.log(`Humidity: ${humidity}`);
+    console.log(`Temperature: ${temp}°F`);
+    console.log(`Min Temp: ${temp_min}°F`);
+    console.log(`Max Temp: ${temp_max}°F`);
+
     // console.log(`Temperature: ${Math.round(temp)}°C`);
     // console.log(`Min Temp: ${Math.round(temp_min)}°C`);
     // console.log(`Max Temp: ${Math.round(temp_max)}°C`);
+
+    document.getElementById("location").textContent = location;
+    document.getElementById("weather").textContent = weather;
+    document.getElementById("wind").textContent = wind;
+    document.getElementById("humidity").textContent = humidity;
+    document.getElementById("temp").textContent = temp;
+    document.getElementById("temp_max").textContent = `${temp_max}°F`;
+    document.getElementById("temp_min").textContent = `${temp_min}°F`;
+    document.getElementById("wind").textContent = `${wind}m/s`;
+    document.getElementById("humidity").textContent = `${humidity}%`;
+    document.getElementById(
+      "iconToday"
+    ).src = `./assets/weathericons/${icon}.png`;
+
+    console.log();
+
+    const promise2 = await fetch(
+      `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&id=${apiKey}&units=imperial`
+    );
+    const foreData = await promise2.json();
+    for (let i = 1; i < 5; i++) {
+    let d1Day = new Date(foreData.list[0].dt_txt);
+
+    switch (d1Day.getDay()) {
+      case 0:
+        d1Day = "Mon";
+        break;
+      case 1:
+        d1Day = "Tue";
+        break;
+      case 2:
+        d1Day = "Wed";
+        break;
+      case 3:
+        d1Day = "Thu";
+        break;
+      case 4:
+        d1Day = "Fri";
+        break;
+      case 5:
+        d1Day = "Sat";
+        break;
+      case 6:
+        d1Day = "Sun";
+        break;
+    }
+
+      console.log(d1Day);
+
+      let d1Average = [];
+      console.log(foreData.list[0].main.temp + foreData.list[1].main.temp);
+      for (let i = 0; i < 8; i++) {
+        d1Average.push(foreData.list[i].main.temp);
+        console.log(foreData.list[i].main.temp);
+      }
+      console.log(d1Average);
+      const sum = d1Average.reduce((partialSum, a) => partialSum + a, 0);
+      console.log(sum);
+      const average = Math.round(sum / 8);
+      console.log(average);
+    }
   }
 
   apiCall();
